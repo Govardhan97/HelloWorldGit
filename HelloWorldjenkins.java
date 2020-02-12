3380a94224a1caba40a990908a1d7df933fd8082
Govardhan Lankalapalli <58773195+Govardhan97@users.noreply.github.com>
2020-02-12 16:40:04 +0530

    Update HelloWorldjenkins.java

--- HelloWorldjenkins.java
+++ HelloWorldjenkins.java
@@ -4,5 +4,6 @@ public static void main(String[] args)
 {
 System.out.println("Hello World java with  Jenkins");
 System.out.println("this is changed file");
+  System.out.println("this is second change lets check it is there in local ");
 }
 }
